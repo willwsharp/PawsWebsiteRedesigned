96f0a297d6741b5e7b50d144e7a9686269160b2a
willwsharp <willwsharp@gmail.com>
2017-10-01 12:29:22 -0400

    fixed #19; also removing some css tidying up

--- src/app/shared/directives/navbar-enlarge.directive.ts
+++ src/app/shared/directives/navbar-enlarge.directive.ts
@@ -1,14 +1,29 @@
-import { Directive, Renderer2, ElementRef, HostListener, Inject } from '@angular/core';
+import { Directive, Renderer2, ElementRef, HostListener, Inject, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
+import { MdDialog } from '@angular/material';
 
 @Directive({
   selector: '[pawsNavbarEnlarge]'
 })
-export class NavbarEnlargeDirective {
+export class NavbarEnlargeDirective implements OnInit {
+
+  private isModalOpen: boolean;
 
   constructor(private renderer: Renderer2,
     private elem: ElementRef,
-    @Inject(DOCUMENT) private document: Document) { }
+    @Inject(DOCUMENT) private document: Document,
+    private mdDialog: MdDialog) { }
+
+
+  ngOnInit() {
+    this.mdDialog.afterOpen.subscribe(() => {
+      this.isModalOpen = true;
+    })
+
+    this.mdDialog.afterAllClosed.subscribe(() => {
+      this.isModalOpen = false;
+    })
+  }
 
   @HostListener("window:scroll")
   onScroll() {
@@ -16,10 +31,13 @@ export class NavbarEnlargeDirective {
 
     let currentOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
-    if (currentOffset > collapseOffset) {
-      this.renderer.addClass(this.elem.nativeElement, 'top-nav-collapse');
-    } else {
-      this.renderer.removeClass(this.elem.nativeElement, 'top-nav-collapse');
+    //don't do anything with the navbar if a modal is open
+    if (!this.isModalOpen) {
+      if (currentOffset > collapseOffset) {
+        this.renderer.addClass(this.elem.nativeElement, 'navbar-reduced');
+      } else {
+        this.renderer.removeClass(this.elem.nativeElement, 'navbar-reduced');
+      }
     }
   }
 
